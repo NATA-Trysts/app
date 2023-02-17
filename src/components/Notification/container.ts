5de67c1ba877243ac25a9d2aa9ca171bc264c09e
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-02-17 14:37:50 +0700

    fix: change z index of container

--- src/components/Notification/container.ts
+++ src/components/Notification/container.ts
@@ -12,7 +12,7 @@ const createContainer = () => {
   element.style.top = '30px'
   element.style.left = '50%'
   element.style.transform = 'translateX(-50%)'
-  element.style.zIndex = '9999'
+  element.style.zIndex = '2147483647'
   document.body.appendChild(element)
 
   return element
