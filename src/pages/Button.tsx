d2da40917d6819bd97e4fc1bf59c03dd3d819739
huutinhlg2d <minkyshinyu@gmail.com>
2023-02-17 16:20:11 +0700

    feat: implement Gradient Button component

---
+++ src/pages/Button.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import { GradientButton } from '@/components/Button'
+
+const ButtonContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  place-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+`
+
+export const Button = () => {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div style={{ backgroundColor: '#212225' }}>
+      <ButtonContainer>
+        <GradientButton disabled onClick={handleClick}>
+          Create Space
+        </GradientButton>
+        <GradientButton onClick={handleClick}>Create Space</GradientButton>
+        <div
+          style={{
+            color: '#FFFFFF',
+            fontWeight: 600,
+            height: '20px',
+            width: '20px',
+            textAlign: 'center',
+          }}
+        >
+          <span>{count}</span>
+        </div>
+      </ButtonContainer>
+    </div>
+  )
+}
