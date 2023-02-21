817e81854bf5d02bcc6fa34936336eadcf3de96e
huutinhlg2d <minkyshinyu@gmail.com>
2023-02-21 11:28:04 +0700

    fix: hot fix logo component v2

--- src/components/Commons/Logo/Logo.tsx
+++ src/components/Commons/Logo/Logo.tsx
@@ -4,16 +4,16 @@ import styled from 'styled-components'
 
 import { ReactComponent as LogoSvg } from '@/assets/icons/logo.svg'
 
-const LogoContainer = styled.div<{ width: number; aspectRatio: string }>`
+const LogoContainer = styled.div<{ width: number; aspectRatio: { x: number; y: number; fraction: string } }>`
   width: ${(props) => `${props.width}px`};
   display: flex;
   position: relative;
-  aspect-ratio: ${(props) => props.aspectRatio};
+  aspect-ratio: ${(props) => props.aspectRatio.fraction};
 
   &:before {
     content: '';
-    width: ${(props) => `${(props.width * 22.55) / 66}px`};
-    aspect-ratio: 22.55/12;
+    width: ${(props) => `${(props.width * props.aspectRatio.y) / props.aspectRatio.x}px`};
+    aspect-ratio: ${(props) => `${props.aspectRatio.y}/12`};
     display: block;
     position: absolute;
     top: 33.33%;
@@ -21,7 +21,7 @@ const LogoContainer = styled.div<{ width: number; aspectRatio: string }>`
 
     background: linear-gradient(90deg, rgba(255, 147, 138, 0.6) 1.24%, rgba(255, 110, 136, 0.6) 42.49%);
     filter: blur(7.13644px);
-    filter: blur(${(props) => `${(props.width * 7.13644) / 66}px`});
+    filter: blur(${(props) => `${(props.width * 7.13644) / props.aspectRatio.x}px`});
   }
 `
 
@@ -37,7 +37,10 @@ export const Logo = ({
   const svgSize = { width: width, height: (width * aspectRatio.y) / aspectRatio.x }
 
   return (
-    <LogoContainer aspectRatio={`${aspectRatio.x}/${aspectRatio.y}`} width={width}>
+    <LogoContainer
+      aspectRatio={{ x: aspectRatio.x, y: aspectRatio.y, fraction: `${aspectRatio.x}/${aspectRatio.y}` }}
+      width={width}
+    >
       {/* <StyleLogo /> */}
       <StyleLogo height={svgSize.height} viewBox={`0 0 ${aspectRatio.x} ${aspectRatio.y}`} width={svgSize.width} />
     </LogoContainer>
