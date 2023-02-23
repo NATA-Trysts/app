83101205ff918f67cca73682ba464e3cc963e78a
Hoang Tien Thinh <85208341+TienThinh-fe@users.noreply.github.com>
2023-02-23 11:23:46 +0700

    feat: navigation panel ⼬ (#23)

---
+++ src/components/Navigation/NavigationProfile.tsx
@@ -0,0 +1,81 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+import { useNavigationPanelStore } from '@/stores/navigationPanel'
+
+import { MyInformationCard } from '../MyInformationCard'
+
+const NavigationProfileContainer = styled.div`
+  width: 100%;
+  background-color: #212225;
+  border-radius: 8px;
+  transition: all 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  pointer-events: auto;
+`
+
+const ProfileCharacter = styled.div`
+  width: 184px;
+  height: 184px;
+  border-radius: 6px;
+  overflow: hidden;
+  margin-bottom: 8px;
+`
+
+const ProfileCharacterCanvas = styled(Canvas)`
+  background-color: white;
+`
+
+// for collapse mode
+const ProfileAvatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`
+
+export const NavigationProfile = () => {
+  // temporary data
+  const userInfo = {
+    avatar: 'https://i.pinimg.com/originals/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg',
+    handler: 'sonha#1234',
+    name: 'Nguyen Son Ha',
+  }
+
+  const isExpanded = useNavigationPanelStore((state) => state.isExpanded)
+  const [isDisplayed, setIsDisplayed] = useState(false)
+
+  // delay display of help title to prevent flickering
+  useEffect(() => {
+    if (isExpanded) {
+      setTimeout(() => setIsDisplayed(true), 160)
+    } else {
+      setIsDisplayed(false)
+    }
+  }, [isExpanded])
+
+  return (
+    <>
+      {isDisplayed ? (
+        <NavigationProfileContainer>
+          <MyInformationCard avatar={userInfo.avatar} handler={userInfo.handler} name={userInfo.name} />
+          <ProfileCharacter>
+            <ProfileCharacterCanvas>
+              <OrbitControls />
+              <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshNormalMaterial />
+              </mesh>
+            </ProfileCharacterCanvas>
+          </ProfileCharacter>
+        </NavigationProfileContainer>
+      ) : (
+        <ProfileAvatar src={userInfo.avatar} />
+      )}
+    </>
+  )
+}
