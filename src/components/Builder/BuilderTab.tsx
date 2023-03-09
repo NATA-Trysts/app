d5b41e6955e0939ffa64bcd319756956a6f97a0a
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-03-09 10:43:09 +0700

    feat: back button at builder panel

--- src/components/Builder/BuilderTab.tsx
+++ src/components/Builder/BuilderTab.tsx
@@ -1,7 +1,9 @@
 import { ReactNode, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { ReactComponent as AllIcon } from '@/assets/icons/all-application.svg'
+import { ReactComponent as ArrowLeft } from '@/assets/icons/arrow-left.svg'
 import { ReactComponent as SmileFaceIcon } from '@/assets/icons/smile-face.svg'
 import { CategoryType, useBuilderStore } from '@/stores'
 
@@ -29,6 +31,26 @@ const Dash = styled.div`
   margin-top: 4px;
 `
 
+const BackWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  margin: 8px 0;
+  padding: 8px;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+
+  :hover {
+    background-color: #212225;
+  }
+`
+
+const Upper = styled.div``
+
+const Lower = styled.div``
+
 type Category = {
   id: number
   icon: ReactNode
@@ -52,6 +74,7 @@ export const BuilderTab = () => {
     },
   ])
   const setSelectedCategory = useBuilderStore((state) => state.setSelectedCategory)
+  const navigate = useNavigate()
 
   const handleClickTab = (id: number) => {
     const newCategories = categories.map((category) => {
@@ -71,6 +94,10 @@ export const BuilderTab = () => {
     setCategories(newCategories)
   }
 
+  const handleBack = () => {
+    navigate('/dashboard', { replace: true })
+  }
+
   useEffect(() => {
     const newSelectedCategory = categories.find((category) => category.isActive === true)
 
@@ -81,18 +108,25 @@ export const BuilderTab = () => {
 
   return (
     <BuilderTabContainer>
-      <CategoryList>
-        {categories.map((category) => (
-          <CategoryItem
-            key={category.id}
-            icon={category.icon}
-            isActive={category.isActive}
-            onClick={() => handleClickTab(category.id)}
-          />
-        ))}
-        <Dash />
-      </CategoryList>
-      <BuilderHelp />
+      <Upper>
+        <BackWrapper onClick={handleBack}>
+          <ArrowLeft />
+        </BackWrapper>
+        <CategoryList>
+          {categories.map((category) => (
+            <CategoryItem
+              key={category.id}
+              icon={category.icon}
+              isActive={category.isActive}
+              onClick={() => handleClickTab(category.id)}
+            />
+          ))}
+          <Dash />
+        </CategoryList>
+      </Upper>
+      <Lower>
+        <BuilderHelp />
+      </Lower>
     </BuilderTabContainer>
   )
 }
