07986973947488b7114de211ac0041a3c2f01d45
Ha S. Nguyen <57525862+sonhaaa@users.noreply.github.com>
2023-04-03 11:25:34 +0700

    fix: character more animations and character config prepare (#102)

--- src/components/Member/BaseCharacter.tsx
+++ src/components/Member/BaseCharacter.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import { useAnimations, useGLTF } from '@react-three/drei'
+import { useAnimations, useGLTF, useTexture } from '@react-three/drei'
 import { useGraph } from '@react-three/fiber'
 import { useEffect, useMemo, useRef } from 'react'
 import { Group } from 'three'
@@ -9,27 +9,51 @@ import { GLTF, SkeletonUtils } from 'three-stdlib'
 type GLTFResult = GLTF & {
   nodes: {
     body: THREE.SkinnedMesh
-    ear: THREE.SkinnedMesh
-    head: THREE.SkinnedMesh
-    neck: THREE.SkinnedMesh
+    hair002: THREE.SkinnedMesh
+    Cube020: THREE.SkinnedMesh
+    Cube020_1: THREE.SkinnedMesh
     mixamorigHips: THREE.Bone
   }
   materials: {
+    body: THREE.MeshStandardMaterial
+    Material: THREE.MeshStandardMaterial
     head: THREE.MeshStandardMaterial
     ear: THREE.MeshStandardMaterial
-    neck: THREE.MeshStandardMaterial
   }
 }
 
 type ActionName =
-  | 'Armature|mixamo.com|Layer0'
-  | 'Armature|mixamo.com|Layer0.002'
+  | 'bow'
+  | 'cheer'
+  | 'clap'
+  | 'dance'
+  | 'discuss'
+  | 'fall'
+  | 'fall.001'
+  | 'fall.003'
+  | 'hit'
+  | 'hit.001'
+  | 'hit.003'
   | 'idle'
   | 'idle.001'
+  | 'idle.002'
+  | 'idle.003'
+  | 'kick'
+  | 'kick.001'
+  | 'kick.002'
+  | 'kick.003'
+  | 'lay'
+  | 'punch'
+  | 'punch.001'
+  | 'punch.003'
   | 'run'
-  | 'running'
+  | 'sit'
+  | 'sit.001'
+  | 'sit.002'
+  | 'sit.003'
+  | 'victory'
   | 'walk'
-  | 'walking'
+  | 'wave'
 type GLTFActions = Record<ActionName, THREE.AnimationAction>
 
 type ModelProps = {
@@ -43,6 +67,10 @@ export function BaseCharacter(props: ModelProps) {
   const { nodes } = useGraph(clone) as GLTFResult
   const { actions } = useAnimations<GLTFActions>(animations, group)
 
+  const [tattooCircle] = useTexture(['/textures/t.tattoo.001.png'], (textures) => {
+    textures.map((t) => (t.flipY = false))
+  })
+
   useEffect(() => {
     props.action && actions[props.action].reset().fadeIn(0.2).play()
 
@@ -54,23 +82,68 @@ export function BaseCharacter(props: ModelProps) {
       <group name="Scene" position={[0, -2, 0]}>
         <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.015}>
           <primitive object={nodes.mixamorigHips} />
+
+          {/* BASE PARTS */}
+          <skinnedMesh geometry={nodes.body.geometry} skeleton={nodes.body.skeleton}>
+            <meshBasicMaterial map={tattooCircle} />
+          </skinnedMesh>
+          <skinnedMesh geometry={nodes.Cube020.geometry} material={materials.head} skeleton={nodes.Cube020.skeleton} />
           <skinnedMesh
-            geometry={nodes.body.geometry}
-            material={materials.head}
-            name="body"
-            skeleton={nodes.body.skeleton}
-          />
-          <skinnedMesh
-            geometry={nodes.ear.geometry}
+            geometry={nodes.Cube020_1.geometry}
             material={materials.ear}
-            name="ear"
-            skeleton={nodes.ear.skeleton}
+            skeleton={nodes.Cube020_1.skeleton}
+          />
+          {/* <skinnedMesh geometry={nodes.neck.geometry} material={nodes.neck.material} skeleton={nodes.neck.skeleton} /> */}
+
+          {/* HAIR PARTS */}
+          {/* <skinnedMesh geometry={nodes.hair001.geometry} material={materials.hair} skeleton={nodes.hair001.skeleton} /> */}
+          <skinnedMesh
+            geometry={nodes.hair002.geometry}
+            material={materials.Material}
+            skeleton={nodes.hair002.skeleton}
+          />
+
+          {/* HAT PARTS */}
+          {/* <skinnedMesh
+            geometry={nodes.hat001.geometry}
+            material={nodes.hat001.material}
+            skeleton={nodes.hat001.skeleton}
           />
           <skinnedMesh
-            geometry={nodes.head.geometry}
-            material={materials.head}
-            name="head"
-            skeleton={nodes.head.skeleton}
+            geometry={nodes.hat002.geometry}
+            material={nodes.hat002.material}
+            skeleton={nodes.hat002.skeleton}
+          /> */}
+
+          {/* UPPER PARTS */}
+          {/* <skinnedMesh
+            geometry={nodes.upper001.geometry}
+            material={nodes.upper001.material}
+            skeleton={nodes.upper001.skeleton}
+          /> */}
+          {/* <skinnedMesh
+            geometry={nodes.upper002.geometry}
+            material={nodes.upper002.material}
+            skeleton={nodes.upper002.skeleton}
+          /> */}
+
+          {/* LOWER PARTS */}
+          <skinnedMesh
+            geometry={nodes.lower001.geometry}
+            material={nodes.lower001.material}
+            skeleton={nodes.lower001.skeleton}
+          />
+          {/* <skinnedMesh
+            geometry={nodes.lower002.geometry}
+            material={nodes.lower002.material}
+            skeleton={nodes.lower002.skeleton}
+          /> */}
+
+          {/* SHOE PARTS */}
+          <skinnedMesh
+            geometry={nodes.shoe001.geometry}
+            material={nodes.shoe001.material}
+            skeleton={nodes.shoe001.skeleton}
           />
         </group>
       </group>
